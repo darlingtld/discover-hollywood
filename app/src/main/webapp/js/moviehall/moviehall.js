4046a9a99b687d29fe22f351d660e6bd33e64523
lingda tang <darlingtld@qq.com>
2015-11-29 12:33:06 +0800

    bug fix

--- app/src/main/webapp/js/moviehall/moviehall.js
+++ app/src/main/webapp/js/moviehall/moviehall.js
@@ -8,16 +8,16 @@ app.controller("MoviehallController", function ($scope, $http, $location) {
     })
 
     $scope.rateMap = [
-        {name: "0.5", value: "0.5"},
-        {name: "1.0", value: "1.0"},
-        {name: "1.5", value: "1.5"},
-        {name: "2.0", value: "2.0"},
-        {name: "2.5", value: "2.5"},
-        {name: "3.0", value: "3.0"},
-        {name: "3.5", value: "3.5"},
-        {name: "4.0", value: "4.0"},
-        {name: "4.5", value: "4.5"},
-        {name: "5.0", value: "5.0"},
+        {name: "0.5 ☆", value: "0.5"},
+        {name: "1.0 ★", value: "1.0"},
+        {name: "1.5 ★☆", value: "1.5"},
+        {name: "2.0 ★★", value: "2.0"},
+        {name: "2.5 ★★☆", value: "2.5"},
+        {name: "3.0 ★★★", value: "3.0"},
+        {name: "3.5 ★★★☆", value: "3.5"},
+        {name: "4.0 ★★★★", value: "4.0"},
+        {name: "4.5 ★★★★☆", value: "4.5"},
+        {name: "5.0 ★★★★★", value: "5.0"},
 
     ]
 
@@ -29,7 +29,7 @@ app.controller("MoviehallController", function ($scope, $http, $location) {
         $http.post("movie/rate", {
             userId: sessionStorage['userId'],
             movieId: $scope.movieIdToRate,
-            rating: $scope.rating,
+            rating: $scope.rating.value,
             timestamp: new Date()
         }).success(function (data) {
             alert("Thanks for your rating");
